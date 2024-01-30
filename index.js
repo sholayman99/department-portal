1a7dbea21be569e50fc2ee3111b67fa70a344a5e
Md Sholayman <mdsholayman998@gmail.com>
2024-01-30 22:57:23 +0600

    initialize the project

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+* Author : Md. Sholayman
+* Description: This is the file where app is running.
+* Date: 30 January  2024.
+* */
+
+const app = require("./app");
+
+
+app.listen(5000,()=>{
+    console.log("App is Running @ 5000")
+})
---
+++ src/routes/api.js
@@ -0,0 +1,13 @@
+/*
+* Author : Md. Sholayman
+* Description: This is the file where router are being hold.
+*  Date: 30 January  2024.
+* */
+
+const express = require("express");
+const router = express.Router();
+
+
+
+
+module.exports=router;
