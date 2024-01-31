cbde8694cb05cbd2d6bdae5c0e3d273ff98948ea
Md Sholayman <mdsholayman998@gmail.com>
2024-01-31 20:55:41 +0600

    file structure created for other controller

---
+++ src/controllers/otherController.js
@@ -0,0 +1,30 @@
+/*
+* Author : Md. Sholayman
+* Description: This file contains all the controller for others request.
+* Date: 30 January  2024.
+* */
+
+//controller for slider
+exports.findSliders = async (req,res)=>{
+
+}
+
+//controller for programme
+exports.findProgramme = async (req,res)=>{
+
+}
+
+//controller for event
+exports.findEvent = async (req,res)=>{
+
+}
+
+//controller for gallery
+exports.findGallery = async (req,res)=>{
+
+}
+
+//controller for event
+exports.findEvent = async (req,res)=>{
+
+}
