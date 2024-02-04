ef7d12ba9aad9beaf699b2e436f96ed88bc03b1a
Md Sholayman <mdsholayman998@gmail.com>
2024-02-04 17:34:23 +0600

    initialize the the client site

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Button} from "keep-react";
+
+const App = () => {
+    return (
+        <div>
+            <Button size="md">Default</Button>
+        </div>
+    );
+};
+
+export default App;
